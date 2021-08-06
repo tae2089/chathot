30169a9bdf6d37a28a5d94c9e803a883a2ca6cd1
tae2089 <tae2089002@gmail.com>
2021-08-06 12:17:07 +0900

    update rest api

--- src/component/chat/Info.js
+++ src/component/chat/Info.js
@@ -4,17 +4,21 @@ import { Loading } from "react-simple-chatbot";
 import axios from "axios";
 const Info = (props) => {
   const [data, setData] = useState("");
+  const [loding, setLoding] = useState(true);
   const steps = useMemo(() => {
     return props.steps;
   }, [props]);
+
   const gotoNext = useCallback(() => {
     props.triggerNextStep();
   }, [props]);
+
   useEffect(() => {
     console.log(steps);
     const getip = async () => {
       const rep = await axios.get("https://api.ipify.org?format=json");
       console.log(rep);
+      setLoding(false);
       setData(JSON.stringify(rep.data.ip));
     };
     Promise.all([getip()]).then(() => {
@@ -22,7 +26,7 @@ const Info = (props) => {
     });
   }, []);
 
-  return <div>{data}</div>;
+  return <div>{loding === true ? <Loading /> : data}</div>;
 };
 
 export default Info;
